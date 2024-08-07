a67de43ea9dea8698c8246af47f8c40d1c9a02f1
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-08-07 08:38:02 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,35 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 const Home = () => {
-  return <Button>test</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
+        <p>Quarta Feira, 07 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-8">
+          <Input placeholder="Faça sau busca...." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home

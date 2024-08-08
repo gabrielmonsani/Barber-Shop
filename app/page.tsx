fc9a207fa060e4ccb20b458686685c85be96960b
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-08-08 13:08:22 -0300

    fix: correct BookingItem component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import { Input } from "./_components/ui/input" // Corrigi o caminho de importação do Input
 import { Button } from "./_components/ui/button"
@@ -64,29 +61,6 @@ const Home = async () => {
 
         {/* Agendamento */}
         <BookingItem />
-
-        <Card className="mt-6">
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">07</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados

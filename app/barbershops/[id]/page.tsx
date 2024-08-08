f3fa8d081491ef46acc4219da8bebba693c0e45c
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-08-08 13:02:09 -0300

    feat: add SidebarSheet component

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -5,6 +5,9 @@ import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { notFound } from "next/navigation"
+import SidebarSheet from "@/app/_components/sidebar-sheet"
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet"
 
 interface BarbershopPageProps {
   params: {
@@ -24,16 +27,16 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   })
 
   if (!barbershop) {
-    return <div>Barbershop not found</div>
+    return notFound()
   }
 
   return (
     <div>
-      {/* IMAGEM*/}
+      {/* IMAGEM */}
       <div className="relative h-[250px] w-full">
         <Image
           alt={barbershop.name}
-          src={barbershop.imageUrl}
+          src={barbershop?.imageUrl}
           fill
           className="object-cover"
         />
@@ -49,36 +52,42 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           </Link>
         </Button>
 
-        <Button
-          size="icon"
-          variant="secondary"
-          className="absolute right-4 top-4"
-        >
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="outline"
+              className="absolute right-4 top-4"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
       </div>
 
+      {/* TÍTULO */}
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
-        <div className="mb-2 flex items-center gap-1">
+        <div className="mb-2 flex items-center gap-2">
           <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop?.address}</p>
         </div>
 
         <div className="flex items-center gap-2">
           <StarIcon className="fill-primary text-primary" size={18} />
-          <p className="text-sm">5,0 (499 avaliaçôes)</p>
+          <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
       </div>
 
-      {/* DESCRIÇÃO*/}
-      <div className="space-y-3 border-b border-solid p-5">
+      {/* DESCRIÇÃO */}
+      <div className="space-y-2 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
 
       {/* SERVIÇOS */}
-      <div className="space-y-3 p-5">
+      <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
         <div className="space-y-3">
           {barbershop.services.map((service) => (

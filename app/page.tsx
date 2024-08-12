5e7c2308a998f9ab4642b994ef3b9a6ad291328d
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-08-12 16:03:45 -0300

    feat: ensure bookings are listed on bookings page

--- app/page.tsx
+++ app/page.tsx
@@ -7,54 +7,76 @@ import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
 
 const Home = async () => {
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
 
   return (
     <div>
-      {/* Header */}
+      {/* header */}
       <Header />
       <div className="p-5">
-        {/* Texto */}
+        {/* TEXTO */}
         <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
         <p>Segunda-feira, 05 de agosto.</p>
-
-        {/* Busca */}
+        {/* BUSCA */}
         <div className="mt-6">
           <Search />
         </div>
-
-        {/* Busca rápida */}
+        {/* BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {quickSearchOptions.map((option) => (
-            <Link
-              href={`/barbershops?service=${option.title}`}
+            <Button
+              className="gap-2"
+              variant="secondary"
               key={option.title}
+              asChild
             >
-              <Button className="gap-2" variant="secondary">
-                {option.imageUrl && typeof option.imageUrl === "string" ? (
-                  <Image
-                    src={option.imageUrl}
-                    width={16}
-                    height={16}
-                    alt={option.title}
-                  />
-                ) : (
-                  <span>Imagem não disponível</span>
-                )}
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={
+                    typeof option.imageUrl === "string"
+                      ? option.imageUrl
+                      : "/default-image.png"
+                  }
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
                 {option.title}
-              </Button>
-            </Link>
+              </Link>
+            </Button>
           ))}
         </div>
-
-        {/* Imagem */}
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -64,8 +86,16 @@ const Home = async () => {
           />
         </div>
 
-        {/* Agendamento */}
-        <BookingItem />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        {/* AGENDAMENTO */}
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
@@ -75,7 +105,6 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
-
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Populares
         </h2>
@@ -88,5 +117,4 @@ const Home = async () => {
     </div>
   )
 }
-
 export default Home
